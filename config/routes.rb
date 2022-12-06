6c7255526ac5155c19d02e0cf216d040888c7712
techEmprez <choiceosobor@gmail.com>
2022-12-06 07:12:32 +0100

    create and add users controllers method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  resources :user only [:index, :show] do
-  resources :post only [:index, :show]
+  resources :users only [:index, :show] do
+  resources :posts only [:index, :show]
   end
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "user#index"
+  root "users#index"
 end
