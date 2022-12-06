9669b14141c1329b3c2e555b05a7ae6ce3f09787
techEmprez <choiceosobor@gmail.com>
2022-12-06 07:10:54 +0100

    add user contollers file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  resources :users only [:index, :show] do
-  resources :posts only [:index, :show]
+  resources :user only [:index, :show] do
+  resources :post only [:index, :show]
   end
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  root "user#index"
 end
