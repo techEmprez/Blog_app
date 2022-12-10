9dae04807a3a99a2145bf3ca51551204822b306f
techEmprez <choiceosobor@gmail.com>
2022-12-10 07:29:54 +0100

    add current users and authentification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @user = User.all
+    @current = current_user
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @post = @user.most_recent_posts
+  end
 end
