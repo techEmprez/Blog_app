2632aa71af1ba9287d65f4c4ecd9a5357a4bc32d
techEmprez <choiceosobor@gmail.com>
2022-12-10 14:59:04 +0100

    styling for post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
-    @post = Post.where(author_id: @user.id).order(created_at: :desc)
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    { @user => @user, @posts => @posts }
   end
 
   def show
-    @post = Post.includes(:comments, comments: [:author]).find(params[:id])
-    @comment = Comment.where(post_id: @post.id).order(created_at: :desc)
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = current_user.posts.new(post_params)
-    @post.comments_counter = 0
-    @post.likes_counter = 0
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to user_posts_path(@post.author_id), notice: 'Post was successfully created.' }
-      else
-        flash[:error] = 'Failed to create post'
-        format.html { redirect_to new_user_post_path }
-      end
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
+    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post = @user.most_recent_posts
+    # @post = @user.most_recent_posts
   end
 end
