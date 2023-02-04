e6a2e180712171d8cca0e66a22284ebb6315eb26
techEmprez <choiceosobor@gmail.com>
2023-02-04 14:12:48 +0100

    add test for post show

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :feature do
   describe 'show page' do
     before(:example) do
-      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+      @user = User.create(name: 'Tom', photo: 'https://cdn.pixabay.com/photo/2023/01/23/17/54/cat-7739291_1280.jpg', bio: 'Teacher from Mexico.', email: 'xyz@gmail.com',
                           posts_counter: 0)
-      @user2 = User.create(name: 'Mohammed', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher Pakistan.',
+      @user2 = User.create(name: 'Lilly', photo: 'https://cdn.pixabay.com/photo/2023/01/23/17/54/cat-7739291_1280.jpg', bio: 'Student from Poland.', email: 'abc@gmail.com',
                            posts_counter: 0)
-      @post = Post.create(author: @user, title: 'Hello One', text: 'This is my first post', likes_counter: 0,
+      @post = Post.create(author: @user, title: 'Hello You', text: 'This is my first post', likes_counter: 0,
                           comments_counter: 0)
       @comment1 = Comment.create(author: @user, post: @post, text: 'I like this post comment one')
       @comment2 = Comment.create(author: @user2, post: @post, text: 'I like this post comment two')
