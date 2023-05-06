1c9c2dd067848c08b9e92569e36d12e4bbe7b0e5
Choice Osobor <choiceosobor@gmail.com>
2023-05-06 18:45:57 +0100

    modify post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,9 @@
-class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+has_many :comments, foreign_key: 'post_id'
+  after_save :update_post_counter
+  
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  after_save :update_post_counter
 
   def recent_comments
     comments.order('created_at Desc').limit(5)
