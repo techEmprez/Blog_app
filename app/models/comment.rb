03eb6248417e4e7c0825b57776a9f5cced01bca7
Choice Osobor <choiceosobor@gmail.com>
2023-05-06 18:00:44 +0100

    fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 
   validates :text, presence: true, length: { maximum: 100 }
 
+  after_save :update_comments_counter
+  
   def update_comments_counter
     post.increment!(:comments_counter)
   end
