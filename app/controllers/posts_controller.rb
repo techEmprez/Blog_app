6ef3c27dd83592f1292d1458424373eb6a821d80
Choice Osobor <choiceosobor@gmail.com>
2023-05-09 13:18:31 +0100

    create post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
-    { @user => @user, @posts => @posts }
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author = current_user
-    if @post.save
-      redirect_to user_posts_path
-    else
-      render :new
-    end
+    @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
   end
 end
