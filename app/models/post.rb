1e476ad6074c9f993b0624a76e537a00bd5ae8f2
Choice Osobor <choiceosobor@gmail.com>
2023-05-13 17:07:37 +0100

    modidy views for recent posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,26 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  after_save :update_posts_counter
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def most_recent_comments
+  def recent_five
     comments.order(created_at: :desc).limit(5)
   end
 
-  after_save :update_post_counter
+  # private
 
-  private
-
-  def update_post_counter
+  def update_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
