7dba493a9c790995b26b18c7f5ef1b804353e58f
Choice Osobor <choiceosobor@gmail.com>
2023-05-13 17:21:16 +0100

    delete duplicate files

--- db/migrate/20221126183655_add_ref_users_and_posts_to_likes.rb
+++
@@ -1,6 +0,0 @@
-class AddRefUsersAndPostsToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :likes, :author, foreign_key: { to_table: :users }, null: false
-    add_reference :likes, :post, foreign_key: true
-  end
-end
