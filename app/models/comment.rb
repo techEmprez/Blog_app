4f83fc77bfda0580b6afc9b0624c17e17e631802
Choice Osobor <choiceosobor@gmail.com>
2023-05-16 21:59:03 +0100

    edits

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,16 +2,8 @@ class Comment < ApplicationRecord
   # after_save :post_comment_counter
   belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: true
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post
 
-  after_save :update_comments_counter
-
-  # private
-
-  def update_comments_counter
-    post.increment!(:comments_counter)
-  end
+  validates :text, presence: true, length: { maximum: 100 }
 
   def post_comment_counter
     post.increment!(:comments_count)
