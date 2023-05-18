da94708efc14c2d568cfb4fc774a09ee195e590f
Choice Osobor <choiceosobor@gmail.com>
2023-05-18 07:16:47 +0100

    add before action for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    @like = Like.new
     @post = Post.find(params[:post_id])
-    @user = User.find(params[:user_id])
-    @like.post = @post
-    @like.author = current_user
+    @like = @post.likes.new(post: @post, author: current_user)
     if @like.save
-      redirect_to user_post_path(@user, @post)
+      redirect_to request.referrer
     else
-      render :new, status: :unprocessable_entity
+      render plain: @like.errors.messages
     end
   end
 
