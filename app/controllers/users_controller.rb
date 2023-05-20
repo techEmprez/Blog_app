5971fde038a6241a56ca8e6136583463d2a9dccc
Choice Osobor <choiceosobor@gmail.com>
2023-05-20 14:34:05 +0100

    add load and authorize for users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
-
+  skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @users = User.all
   end
 
   def show
-    @post = Post.new
     @user = User.find(params[:id])
+    puts @user.recent_posts.inspect
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      flash[:success] = 'Successfully created...'
+      redirect_to users_path
+    else
+      flash.now[:alert] = "Couldn't be deleted"
+    end
   end
 end
